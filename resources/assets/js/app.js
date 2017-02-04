de8b1e1875ac4de8c82217ab4d08c8742ce7b657
Arnoldas29 <jomafifi@yahoo.com>
2017-02-05 01:59:05 +0200

    core 6

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,7 +1,7 @@
 
 /**
  * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
+ * include Vue and Vue Resource. This gives a great starting point for
  * building robust, powerful web applications using Vue and Laravel.
  */
 
@@ -9,8 +9,8 @@ require('./bootstrap');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
+ * the body of the page. From here, you may begin adding components to
+ * the application, or feel free to tweak this setup for your needs.
  */
 
 Vue.component('example', require('./components/Example.vue'));
