0fff4d2b4978a3608b3993acd7e94e7b0b211123
Arnoldas <dev@smarttaxi.lt>
2019-01-02 22:03:45 +0200

    five

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('send:birthday-email')->daily();
-        $schedule->command('send:mail')->hourly();
+        $schedule->command('send:mail')->everyFiveMinutes();
     }
 
     /**
